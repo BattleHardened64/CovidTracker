2a3bd01c2539f45ae9d8a63b3288322a651d4445
David <djtollett007@gmail.com>
2022-01-31 15:26:00 -0600

    Code Updated to start CSV work

--- covidData.py
+++ covidData.py
@@ -6,11 +6,11 @@ import requests
 
 # Source: https://www.askpython.com/python/examples/pull-data-from-an-api
 
-response_API = requests.get("https://api.covidactnow.org/v2/states.csv?apiKey=0d2a2b37b43e4eeabc6733eb04979486") # connects to the API
+response_API = requests.get("https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_daily_reports/01-01-2022.csv") # connects to the API
 data = response_API.text #gets the data from the API
 if(data):
     print("yee, data!")
-json.loads(data) # parses the data into a JSON format
-# needs to add code to store the json somewhere
 
-#data.to_csv('/Users/kt/Documents/JSON Project/CovidData.csv', encoding='utf-8', index=False)
+csv = pd.read_csv(data)
+#print(type(data))
+csv.to_csv('/Users/djtol/Downloads/CovidData.csv', encoding='utf-8', index=False)
