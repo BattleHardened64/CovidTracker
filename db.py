d12bc159582d79e2de8b8617f31352d385f264d9
BattleHardened <ewilliamstn001@gmail.com>
2022-02-03 11:21:10 -0600

    Added In depth comments as well as scripts to create tables.

--- db.py
+++ db.py
@@ -1,2 +1,30 @@
 import mysql.connector
 
+#-------------------------------#
+#Eli Williams
+#Database Initialization
+#Last Worked on by E. Williams
+#2/3/2022
+#-------------------------------#
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="sampleuser",
+    password="samplepassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+    print(x)
+
+#ODBC drivers will need to be downloaded 
+
+#---------------------------------#
+#Eli Williams
+#Creating Tables for the Database
+#Last Worked on by E. Williams
+#2/3/2022
+#---------------------------------#
+
