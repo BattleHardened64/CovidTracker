059d9f473d94603825302f04559fc3024b7ce786
TheLastMillennial <contactTheLastM@gmail.com>
2022-02-03 10:51:27 -0600

    Update pullCovidData.py
    Program will now get the link for every page of covid data.

--- pullCovidData.py
+++ pullCovidData.py
@@ -1,16 +1,28 @@
-import csv
-import math
-import pandas
-import chardet
-import numpy as np
+import pandas as pd
+import requests
 import dateutil
-import random
-import uuid
+from datetime import timedelta, date
 
-def fn_rd_data(filename):
-    data=pandas.read_csv(filename)
-    return data
 
-fname = "/Users/kt/Documents/JSON Project/01-30-2022.csv" # change to whatever your file is named
-result = fn_rd_data(fname)
-print(result)
+#go through each date sequentially from start to end date
+def daterange(start_date, end_date):
+    for n in range(int ((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+
+#YYYY MM DD
+#todo: set real date
+start_date = date(2020, 1, 1)
+end_date = date.today()
+
+#loop through every date until we get to todays date
+for single_date in daterange(start_date, end_date):
+    datestr=single_date.strftime("%m-%d-%Y")
+    #This is the url to pull data from the github
+    url = r'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/'+datestr+'.csv' 
+    #Here is where you would store the data to sql
+    print(url)
+
+
+
