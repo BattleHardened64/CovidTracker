d4107b8ea2308ca8bfb1cbb8fb5bc30a35608895
BattleHardened <ewilliamstn001@gmail.com>
2022-02-08 01:07:41 -0600

    updated functions and added a new county input function.  Currently stuck on filtering data in python 2/8/22 1:07am, will update tomorrow

--- db.py
+++ db.py
@@ -39,21 +39,20 @@ for x in mycursor:
 #Further Comments: Does this code run on the creation of the instance of the website? 
 #or does this run once something has been clicked? 
 #
-#Currently there are 3 tables, Country, State, County
-mycursor.execute("CREATE TABLE Country (Cases INT, Deaths INT, Recovered INT, Fatality Rate DOUBLE, Recovery Rate, DOUBLE)")
-mycursor.execute("CREATE TABLE State (Cases INT, Deaths INT, Recovered INT, Fatality Rate DOUBLE, Recovery Rate, DOUBLE)")
-mycursor.execute("CREATE TABLE County (Cases INT, Deaths INT, Recovered INT, Fatality Rate DOUBLE, Recovery Rate, DOUBLE)")
+#Currently there are 3 tables, Country, State, County  --- Outdated
+mycursor.execute("CREATE TABLE Location (County VARCHAR(255), State VARCHAR(255), Country VARCHAR(255), Latitude DOUBLE, Longitude DOUBLE, Confirmed INT, Deaths INT, Recovered INT, Active INT, Incident_Rate DOUBLE, Fatality_Rate DOUBLE)")
 
 
+#****** THIS FUNCTION MAY BE IRRELEVANT *******#
 #---------------------------------#
 #Eli Williams
-#Data Parsing before database input
+#Data Parsing before database input 
 #Last Worked on by E. Williams
 #2/7/2022
 #---------------------------------#
-def data_parser():
+#def data_parser():
     #pull data from csv file. keyword usecols(pandas) only reads explicitly named columns, all others are ignored.
-    data = pd.read_csv("data.csv", usecols = 'Admin2' 'Province_State' 'Country_Region' 'Lat' 'Long_' 'Confirmed' 'Deaths' 'Recovered' 'Active' 'Incident_Rate' 'Case_Fatality_Ratio')
+data = pd.read_csv("data.csv", usecols = 'Admin2' 'Province_State' 'Country_Region' 'Lat' 'Long_' 'Confirmed' 'Deaths' 'Recovered' 'Active' 'Incident_Rate' 'Case_Fatality_Ratio')
     #admin2 is county, province_state is state, country is country
     #if admin2 and province state are null, we are looking at the country
     #if province_state has a value, we are looking at a state within a country.
@@ -61,4 +60,16 @@ def data_parser():
     #State data must be calculated using the county data
     #country data must be calculated using the state data.
     #insert into database.
-    sql_insert = "INSERT INTO "
+
+#---------------------------------#
+#Eli Williams
+#County input (All fields are calculated)
+#Last Worked on by E. Williams
+#2/7/2022
+#---------------------------------#
+def county_input():
+    for x in data:
+        if(data.Admin2 == None)
+    sql_insert = "INSERT INTO Location (County, State, Country, Latitude, Longitude, Confirmed, Deaths, Recovered, Active, Incident_Rate, Fatality_Rate) VALUES (%s, %s, %s, %f, %f, %d, %d, %d, %d, %f, %f)"
+
+
